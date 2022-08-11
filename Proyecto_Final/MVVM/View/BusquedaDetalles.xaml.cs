5db5e0113f19a11b7e223e87eb467a101c2626e7
Pasante6 desa <p6@rgtdom.local>
2022-08-11 09:06:23 -0500

    Cree los Templates de las listviews

--- Proyecto_Final/MVVM/View/BusquedaDetalles.xaml.cs
+++ Proyecto_Final/MVVM/View/BusquedaDetalles.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Proyecto_Final.Helpers;
+using Proyecto_Final.Models;
 
 namespace Proyecto_Final.MVVM.View
 {
@@ -22,7 +24,38 @@ namespace Proyecto_Final.MVVM.View
     {
         public BusquedaDetalles()
         {
+            Task.Run(async () => await SearchHelper.GetTokenAsync());
             InitializeComponent();
+        }
+
+        private void etesech(object sender, KeyEventArgs e)
+        {
+            if (Artista.Text == string.Empty)
+            {
+                ListArtist.ItemsSource = null;
+                return;
+            }
+            var result = SearchHelper.SearchArtistOrSong(Artista.Text);
+
+            if (result == null)
+            {
+                return;
+            }
+            var listArtist = new List<SpotifyArtist>();
+
+            foreach (var item in result.artists.items)
+            {
+                listArtist.Add(new SpotifyArtist()
+                {
+                    ID = item.id,
+                    Image = item.images.Any() ? item.images[0].url : "https://w7.pngwing.com/pngs/973/860/png-transparent-anonym-avatar-default-head-person-unknown-user-user-pictures-icon-thumbnail.png",
+                    Name = item.name,
+                    Popularity = $"{item.popularity}%",
+                    Followers = $"{item.followers.total.ToString("N")} seguidores",
+
+                });
+            }
+            ListArtist.ItemsSource = listArtist;
         }
     }
 }
--- Proyecto_Final/MVVM/View/BusquedaImagen.xaml.cs
+++ Proyecto_Final/MVVM/View/BusquedaImagen.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Proyecto_Final.Helpers;
+using Proyecto_Final.Models;
+
 
 namespace Proyecto_Final.MVVM.View
 {
@@ -22,7 +25,38 @@ namespace Proyecto_Final.MVVM.View
     {
         public BusquedaImagen()
         {
+            Task.Run(async () => await SearchHelper.GetTokenAsync());
             InitializeComponent();
+        }
+
+        private void etesech(object sender, KeyEventArgs e)
+        {
+            if (Artista.Text == string.Empty)
+            {
+                ListArtist.ItemsSource = null;
+                return;
+            }
+            var result = SearchHelper.SearchArtistOrSong(Artista.Text);
+
+            if (result == null)
+            {
+                return;
+            }
+            var listArtist = new List<SpotifyArtist>();
+
+            foreach (var item in result.artists.items)
+            {
+                listArtist.Add(new SpotifyArtist()
+                {
+                    ID = item.id,
+                    Image = item.images.Any() ? item.images[0].url : "https://w7.pngwing.com/pngs/973/860/png-transparent-anonym-avatar-default-head-person-unknown-user-user-pictures-icon-thumbnail.png",
+                    Name = item.name,
+                    Popularity = $"{item.popularity}%",
+                    Followers = $"{item.followers.total.ToString("N")} seguidores",
+
+                });
+            }
+            ListArtist.ItemsSource = listArtist;
         }
     }
 }
